eac1ef76dc604a41aba39b49eb34adc0916537ea
Parham <95162943+Parham1258@users.noreply.github.com>
2024-06-24 15:33:01 +0330

    language

--- Config.py
+++ Config.py
@@ -1,11 +1,11 @@
 # Config file for VPS Control Panel
-Company="Company" # Company name, will show before the VPS Control Panel text
+Company="Company" # Company name, will show before the VPS Control Panel text (Currently only shows for english language)
 
 Host="0.0.0.0" # Host (127.0.0.1 for local access only, 0.0.0.0 for remote access)
-Port=50009 # Port
-Threads=6
+Port=50009 # Access port
+Threads=6 # Number of requests at the same time (if more than the number here they will be placed on queue)
 
-noVNC_Host="hosting.parham200.ir"
+noVNC_Host="hosting.parham200.ir" # Host url for noVNC
 
 # For NAT Loopback:
 Lan_IP="192.168.0.213"
@@ -18,9 +18,11 @@ Cooldown_Toggle=True
 Cooldown=1 # How many seconds before another API action is allowed
 Cooldown_If_In_Cooldown=2 # How many seconds are added to the current Cooldown because you're already on cooldown
 
-If_No_Permission_404=True # If you don't have permission to view a server, the API (Panel already does that) will return 404 (Not Found) instead of 401 (Unauthorized)
+If_No_Permission_404=True # If you don't have permission to view a server, return 404 (Not Found) instead of 401 (Unauthorized)
 
 VMRUN_PATH = r'"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"'
+
+Language=None # ISO language code (example: en for English) defaults to en if invalid, None for auto select from user browser headers
 
 # Developer Options:
 Debug=False
--- Utils.py
+++ Utils.py
@@ -1,13 +1,16 @@
 from flask import render_template as render_template_
 import json
-from Config import Company
+from Config import Company, Language
 
 langs=["es", "fa"]
 class language:
     def __init__(self, request): self.request=request
     def translate(self, text):
-        if "Lang" not in self.request.cookies or self.request.cookies["Lang"] not in langs: d="en"
-        else: d=self.request.cookies["Lang"]
+        if Language==None:
+            if "Lang" not in self.request.cookies or self.request.cookies["Lang"] not in langs: d="en"
+            else: d=self.request.cookies["Lang"]
+        elif Language in langs: d=Language
+        else: d="en"
         b=dict(sorted(json.load(open(f"Lang/{d}.json", encoding="utf8+")).items(), key=lambda x: len(x[0]), reverse=True))
         for i in b:
             if i in text: text=text.replace(i, b[i])
